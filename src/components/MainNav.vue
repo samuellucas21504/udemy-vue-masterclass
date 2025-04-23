fb5c9d9fdd63208195f1ea0e2aae3add452387e7
Samuel Lucas Rodrigues <samuellucasrdg@gmail.com>
2025-04-23 18:01:31 -0300

    feat: Implementa MainNav
    - Corrige tailwindcss
    - Implementa cores do tema

---
+++ src/components/MainNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 bg-white w-full h-16">
+      <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
+        <a href="/" class="flex h-full items-center text-xl">SLR Careers</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default { name: 'MainNav' }
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
     },
   },
 })
